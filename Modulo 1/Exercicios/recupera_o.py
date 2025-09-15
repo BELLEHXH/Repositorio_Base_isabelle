f0def556af414939d3b058fb0399a18de07d36cc
BELLEHXH <ISABELLEARRUDA352@GMAIL.COM>
2025-09-15 11:18:23 -0300

    Create recupera_o.py

---
+++ Modulo 1/Exercicios/recupera_o.py
@@ -0,0 +1,12 @@
+opção = "s"
+print(f"ola\n")
+while opção == "s"
+   nome = (input(f"qual e o seu nome?\n") )
+   numero = int (input(f"degite a porra de  um numero inteiro:\n") )
+   if numero % 2 == 0:
+     print (f"{nome}, a porra do numero {numero} escolhido e par!\n")
+   else:
+     print (f"{nome},a porra do numero {numero} escolhido e impar!\n")
+   opção = (input(f" deseja verificar outro numero caralho?\ndigite s- sim\n digite ss- não\n") )
+   opção == "ss"
+   print (f"acabou ! \n obrigada,volte sempre!\n")
